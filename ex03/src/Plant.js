2d7f874fe87c2fc90e516395b3db2d4fa69e90f5
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-30 13:52:24 +0200

    Added ex03, ex04

---
+++ ex03/src/Plant.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Plant extends React.Component {
+    constructor(props) {
+
+        super(props);
+        this.state = {
+            type: "Flowering plant"
+        };
+    }
+    render() {
+        return (
+            <div>
+                <h2>Plant type is {this.state.type}</h2>
+           <button type="button" onClick={this.changeType}>
+               Change type
+           </button>
+            </div>
+        );
+    }
+}
+
+export default Plant;
