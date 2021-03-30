11d7622d0e220521d3b49227011a3d39a5bf9e32
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-30 15:27:30 +0200

    Added ex05

---
+++ ex05/src/SimpleForm.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class SimpleForm extends React.Component {
+   constructor(props) {
+       super(props);
+       this.state = { name: 'Yoda'};
+   }
+   onChangeHanler = (event) => {
+       this.setState({name: event.target.value});
+   }
+   render() {
+       return (
+<form>
+    <h1>Name: {this.state.name}</h1>
+    <p>First name:</p>
+    <input 
+    type='text'
+    onChange={this.onChangeHandler}/>
+</form>
+       );
+   }
+
+
+}
+
+export default SimpleForm;
